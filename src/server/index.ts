0ffff1bd3e2fa4fd1018d01677180d8f9e81deb4
Leonard Scheidemantel <leonard@lsindustries.de>
2023-05-21 20:23:37 +0200

    feat: temporarily log whole request

--- src/server/index.ts
+++ src/server/index.ts
@@ -12,9 +12,9 @@ import { routesConfig } from './routes-config';
  */
 const port = config.port || 8080;
 const app = express();
+app.use(logRequest);
 app.use(bodyParser.json());
 app.use(cors({ origin: true }));
-app.use(logRequest);
 routesConfig(app);
 
 /**
